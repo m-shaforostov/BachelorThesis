6b5e481a5c66a93298ed134ae115107da45aa245
maksym <maxim.shaforostov@gmail.com>
2026-04-25 17:23:22 +0200

    running devise is changed 'cuda:0' -> device (variable); saving recurrent model with correct filename

--- rec_vit_model/pckgs/networks/network_utils.py
+++ rec_vit_model/pckgs/networks/network_utils.py
@@ -30,17 +30,18 @@ def load_trained_network(name, dataset, n_loops, run_no=0, pretrained=False, dev
                                        num_heads=num_heads)
 
     if name == 'extra_tiny':
-        model_path = NETWORKS_DIR + f'/{dataset}/{sub_folder}/{ds_info[dataset][1]}_{name}_k_{1}_run_{run_no}.pth'
+        model_path = NETWORKS_DIR + f'/{dataset}/{sub_folder}/{ds_info[dataset][1]}_{name}_k_{n_loops}_run_{run_no}.pth'
     elif name == 'tiny':
         p = '_pretrained' if pretrained else ''
         m2 = '_method2' if method2 else ''
         r1000 = '_reg_1000' if reg_1000 else ''
         on_off = '_on_off' if on_off else ''
-        model_path = NETWORKS_DIR + f'/{dataset}/{sub_folder}/{ds_info[dataset][1]}_{name}{p}_k_{1}_run_{run_no}' \
+        model_path = NETWORKS_DIR + f'/{dataset}/{sub_folder}/{ds_info[dataset][1]}_{name}{p}_k_{n_loops}_run_{run_no}' \
                                     f'{m2}{r1000}{on_off}{suffix}.pth'
     else:
         raise ValueError(f'Model {name} not recognized!')
-    model.load_state_dict(torch.load(model_path, map_location='cuda:0'))
+    # TODO: map_location=device -> map_location='cuda:0'
+    model.load_state_dict(torch.load(model_path, map_location=device))
     model.to(device)
     print(f"Model loaded on {device}")
 
